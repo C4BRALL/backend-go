30c88f33e7eb0aa85aebc204646902b08323c8fa
João Cabral <joao.cabral@mobtelecom.com.br>
2023-10-12 13:43:57 -0300

    :sparkles: :StoreDB

--- src/internal/infra/db/seller_db.go
+++ src/internal/infra/db/seller_db.go
@@ -5,19 +5,19 @@ import (
 	"gorm.io/gorm"
 )
 
-type Seller struct {
+type SellerDB struct {
 	DB *gorm.DB
 }
 
-func NewSeller(db *gorm.DB) *Seller {
-	return &Seller{DB: db}
+func NewSeller(db *gorm.DB) *SellerDB {
+	return &SellerDB{DB: db}
 }
 
-func (s *Seller) Create(seller *entity.Seller) error {
+func (s *SellerDB) Create(seller *entity.Seller) error {
 	return s.DB.Create(seller).Error
 }
 
-func (s *Seller) FindByEmail(email string) (*entity.Seller, error) {
+func (s *SellerDB) FindByEmail(email string) (*entity.Seller, error) {
 	var seller entity.Seller
 	if err := s.DB.Where("email = ?", email).First(&seller).Error; err != nil {
 		return nil, err
---
+++ src/internal/infra/db/store_db.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"github.com/backend/src/internal/entity"
+	"gorm.io/gorm"
+)
+
+type StoreDB struct {
+	DB *gorm.DB
+}
+
+func NewStore(db *gorm.DB) *StoreDB {
+	return &StoreDB{DB: db}
+}
+
+func (s *StoreDB) Create(store *entity.Store) error {
+	return s.DB.Create(store).Error
+}
+
+func (s *StoreDB) FindById(id string) (*entity.Store, error) {
+	var store entity.Store
+	err := s.DB.First(&store, "id = ?", id).Error
+	return &store, err
+}
+
+func (s *StoreDB) Update(store *entity.Store) error {
+	_, err := s.FindById(string(store.ID.String()))
+	if err != nil {
+		return err
+	}
+	return s.DB.Save(store).Error
+}
+
+func (s *StoreDB) Delete(id string) error {
+	store, err := s.FindById(id)
+	if err != nil {
+		return err
+	}
+	return s.DB.Delete(store).Error
+}
+
+func (s *StoreDB) FindAll(page, limit int, sort string) ([]entity.Store, error) {
+	var stores []entity.Store
+	var err error
+	if sort != "" && sort != "desc" && sort != "asc" {
+		sort = "desc"
+	}
+	if page != 0 && limit != 0 {
+		err = s.DB.Limit(limit).Offset((page - 1) * limit).Order("createdAt " + sort).Find(&stores).Error
+	} else {
+		err = s.DB.Order("createdAt " + sort).Find(&stores).Error
+	}
+	return stores, err
+}
