ea938905f810ee7098f061e1242fcf5f063b75da
João Cabral <joao.cabral@mobtelecom.com.br>
2023-10-11 22:29:21 -0300

    :white_check_mark: :add TestSellerValidate and Failing fields when required

--- src/test/internal/entity/seller_test.go
+++ src/test/internal/entity/seller_test.go
@@ -24,3 +24,40 @@ func TestSeller_ValidatePassword(t *testing.T) {
 	assert.False(t, seller.ValidatePassword("passphrase3"))
 	assert.NotEqual(t, "passphrase", seller.Password)
 }
+
+func TestSellerWhenNameIsRequired(t *testing.T) {
+	s, err := entity.NewSeller("", "teste@mail.com", "12312312332", "passphrase", "85912341234")
+	assert.Nil(t, s)
+	assert.Equal(t, entity.ErrNameIsRequired, err)
+}
+
+func TestSellerWhenEmailIsRequired(t *testing.T) {
+	s, err := entity.NewSeller("teste", "", "12312312332", "passphrase", "85912341234")
+	assert.Nil(t, s)
+	assert.Equal(t, entity.ErrEmailIsRequired, err)
+}
+
+func TestSellerWhenDocumentIsRequired(t *testing.T) {
+	s, err := entity.NewSeller("teste", "teste@mail.com", "", "passphrase", "85912341234")
+	assert.Nil(t, s)
+	assert.Equal(t, entity.ErrDocumentIsRequired, err)
+}
+
+func TestSellerWhenPasswordIsRequired(t *testing.T) {
+	s, err := entity.NewSeller("teste", "teste@mail.com", "12312312332", "", "85912341234")
+	assert.Nil(t, s)
+	assert.Equal(t, entity.ErrPasswordIsRequired, err)
+}
+
+func TestSellerWhenPhoneIsRequired(t *testing.T) {
+	s, err := entity.NewSeller("teste", "teste@mail.com", "12312312332", "passphrase", "")
+	assert.Nil(t, s)
+	assert.Equal(t, entity.ErrPhoneIsRequired, err)
+}
+
+func TestSellerValidate(t *testing.T) {
+	s, err := entity.NewSeller("teste", "teste@mail.com", "12312312332", "passphrase", "85912341234")
+	assert.Nil(t, err)
+	assert.NotNil(t, s)
+	assert.Nil(t, s.Validate())
+}
